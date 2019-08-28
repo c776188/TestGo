65e8b4b731c02270eaa58e51c9e83fadec8c21b0
林峻毅 <lin@linjunyide-MacBook-Air.local>
2019-08-28 21:11:58 +0800

    Test v9

--- TestRouter/README.md
+++ TestRouter/README.md
@@ -1,1 +1,5 @@
 Reference https://segmentfault.com/a/1190000014875956
+Github https://github.com/corylanou/tns-restful-json-api
+
+post this to add obj
+curl -H "Content-Type: application/json" -d '{"name": "New Todo"}' http://localhost:8080/todos
--- TestRouter/routers.go
+++ TestRouter/routers.go
@@ -1,51 +1,27 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
-
-type Route struct {
-    Name        string
-    Method      string
-    Pattern     string
-    HandlerFunc http.HandlerFunc
-}
-
-type Routes []Route
 
 func NewRouter() *mux.Router {
 
-    router := mux.NewRouter().StrictSlash(true)
-    for _, route := range routes {
-        router.
-            Methods(route.Method).
-            Path(route.Pattern).
-            Name(route.Name).
-            Handler(route.HandlerFunc)
-    }
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		var handler http.Handler
 
-    return router
-}
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
 
-var routes = Routes{
-    Route{
-        "Index",
-        "GET",
-        "/",
-        Index,
-    },
-    Route{
-        "TodoIndex",
-        "GET",
-        "/todos",
-        TodoIndex,
-    },
-    Route{
-        "TodoShow",
-        "GET",
-        "/todos/{todoId}",
-        TodoShow,
-    },
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+
+	}
+
+	return router
 }
